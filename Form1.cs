4d92d91e7186a905d9e3182646b7d072084bfe14
Maira Vecele <mairavecele6@gmail.com>
2020-05-23 18:01:15 +0300

    version 18:00

--- Form1.cs
+++ Form1.cs
@@ -41,6 +41,12 @@ namespace TicTacToe
         private void Player_Click(object sender, EventArgs e)
         {
             Label label = (Label)sender;
+            //so we can not change the !empty label
+            if(label.Text != string.Empty)
+            {
+                return;
+            }
+
             if (XplayerTurn)
             {
                 label.Text = "X";
@@ -49,7 +55,39 @@ namespace TicTacToe
             {
                 label.Text = "O";
             }
+            CheckForWin();
             XplayerTurn = !XplayerTurn;
+        }
+
+        private void CheckForWin()
+        {
+            if (
+                  (label1.Text == label2.Text && label2.Text == label3.Text && label1.Text != string.Empty) ||
+                  (label4.Text == label5.Text && label5.Text == label6.Text && label4.Text != string.Empty) ||
+                  (label7.Text == label8.Text && label8.Text == label9.Text && label7.Text != string.Empty) ||
+                  (label1.Text == label4.Text && label4.Text == label7.Text && label1.Text != string.Empty) ||
+                  (label2.Text == label5.Text && label5.Text == label8.Text && label2.Text != string.Empty) ||
+                  (label3.Text == label6.Text && label6.Text == label9.Text && label3.Text != string.Empty) ||
+                  (label1.Text == label5.Text && label5.Text == label9.Text && label1.Text != string.Empty) ||
+                  (label3.Text == label5.Text && label5.Text == label7.Text && label3.Text != string.Empty)
+               )
+            {
+                GameOver();
+            }
+        }
+
+        private void GameOver()
+        {
+            string winner;
+            if (XplayerTurn)
+            {
+                winner = "X";
+            }
+            else
+            {
+                winner = "O";
+            }
+            MessageBox.Show(winner + "wins!");
         }
     }
 }
